7e112bc336bc6c4a56ddef6311042acf108ef7cf
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-07-10 19:54:10 +0200

    search elements by selectors

--- cypress-course/cypress/e2e/first-test.cy.js
+++ cypress-course/cypress/e2e/first-test.cy.js
@@ -1,8 +1,15 @@
 /// <reference types="Cypress" />
 
 describe('template spec', () => {
-  it('passes', () => {
+  it('visit test', () => {
     cy.visit('http://localhost:5173/');
+  });
+  
+  it('should have div container', () => {
     cy.get('div').should('have.length', 1);
+  });
+
+  it('should have button element inside card block', () => {
+    cy.get('div.card button').should('have.length', 1);
   });
 })
