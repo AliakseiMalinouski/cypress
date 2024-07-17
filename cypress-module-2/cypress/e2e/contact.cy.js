729373963d711ad142c28cff634fb05e63aad5e6
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-07-17 19:45:06 +0200

    then method

--- cypress-module-2/cypress/e2e/contact.cy.js
+++ cypress-module-2/cypress/e2e/contact.cy.js
@@ -6,4 +6,10 @@ describe(('contact fields tests'), () => {
         cy.get('[data-cy="contact-btn-submit"]').as('submitButton');
         cy.get('@submitButton').should('have.attr', 'type');
     });
+    it('then method', () => {
+        cy.visit('http://localhost:5173/');
+        cy.get('[data-cy="contact-btn-submit"]').then((element) => {
+            expect(element).to.not.have.attr('disabled');
+        });
+    });
 });
