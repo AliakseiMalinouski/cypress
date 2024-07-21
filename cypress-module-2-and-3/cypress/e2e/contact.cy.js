b94f7a6644e7979eff44885820c6c5e71bf54d7e
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-07-21 15:20:04 +0200

    set global base url

--- cypress-module-2-and-3/cypress/e2e/contact.cy.js
+++ cypress-module-2-and-3/cypress/e2e/contact.cy.js
@@ -1,11 +1,11 @@
 describe(('contact fields tests'), () => {
     it('button submit as', () => {
-        cy.visit('http://localhost:5173/');
+        cy.visit('/');
         cy.get('[data-cy="contact-btn-submit"]').as('submitButton');
         cy.get('@submitButton').should('have.attr', 'type');
     });
     it('then method', () => {
-        cy.visit('http://localhost:5173/');
+        cy.visit('/');
         cy.get('[data-cy="contact-btn-submit"]').then((element) => {
             expect(element).to.not.have.attr('disabled');
         });
--- cypress-module-2-and-3/cypress/e2e/navigation.cy.js
+++ cypress-module-2-and-3/cypress/e2e/navigation.cy.js
@@ -1,6 +1,6 @@
 describe('pages navigation', { browser: 'firefox', defaultCommandTimeout: 5000 }, () => {
     it('should navigate between pages', () => {
-        cy.visit('http://localhost:5173/');
+        cy.visit('/');
         cy.get('[data-cy="header-about-link"]').click();
         cy.location('pathname').should('equal', '/about');
         cy.go('back').location('pathname').should('equal', '/');
