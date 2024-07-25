5d11933888d543896571aa5979d4b80452a82eed
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-07-25 18:19:57 +0200

    calls fake

--- cypores-module-4/cypress/e2e/location.cy.js
+++ cypores-module-4/cypress/e2e/location.cy.js
@@ -4,7 +4,17 @@ describe('share location', () => {
   it('should fetch the user location', () => {
     cy.visit('/').then((win) => {
       if('navigator' in win) {
-        cy.stub(window.navigator.geolocation, 'getCurrentPosition').as('getUserPosition');
+        cy.stub(window.navigator.geolocation, 'getCurrentPosition').as('getUserPosition')
+        .callsFake((cb) => {
+          setTimeout(() => {
+            cb({
+              coords: {
+                latitude: 0.1,
+                longitude: 0.1,
+              }
+            });
+          }, 100);
+        });
       }
     });
     cy.get('[data-cy="get-loc-btn"]').click();
