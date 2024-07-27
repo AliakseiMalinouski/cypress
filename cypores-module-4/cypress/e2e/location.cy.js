09e05303ad980254ca12b4c6b1b15d8701145e02
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-07-27 17:27:21 +0200

    stub with return values

--- cypores-module-4/cypress/e2e/location.cy.js
+++ cypores-module-4/cypress/e2e/location.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('share location', () => {
-  it('should fetch the user location', () => {
+  beforeEach(() => {
     cy.visit('/').then((win) => {
       if('navigator' in win) {
         cy.stub(window.navigator.geolocation, 'getCurrentPosition').as('getUserPosition')
@@ -15,9 +15,15 @@ describe('share location', () => {
             });
           }, 100);
         });
+        cy.stub(window.navigator.clipboard, 'writeText')
+        .as('copyToClipboard')
+        .resolves();
       }
     });
+  })
+  it('should fetch the user location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserPosition').should('have.been.called');
+    cy.get('@copyToClipboard').should('have.been.called');
   });
 });
