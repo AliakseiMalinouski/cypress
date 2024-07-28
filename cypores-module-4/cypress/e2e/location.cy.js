6ec2ede2ec7237dbe58b0a045488fa2652564c9e
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-07-28 15:05:14 +0200

    clock and tick

--- cypores-module-4/cypress/e2e/location.cy.js
+++ cypores-module-4/cypress/e2e/location.cy.js
@@ -2,6 +2,7 @@
 
 describe('share location', () => {
   beforeEach(() => {
+    cy.clock();
     cy.fixture('user_location.json').as('userLocation');
     cy.visit('/').then((win) => {
       cy.get('@userLocation').then((params) => {
@@ -16,6 +17,10 @@ describe('share location', () => {
           .as('copyToClipboard')
           .resolves();
         }
+        if('localStorage' in win) {
+          cy.spy(win.localStorage, 'setItem').as('setStore');
+          cy.spy(win.localStorage, 'getItem').as('getStore');
+        }
       });
     });
   })
@@ -23,5 +28,7 @@ describe('share location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserPosition').should('have.been.called');
     cy.get('@copyToClipboard').should('have.been.called');
+    cy.get('@setStore').should('have.been.called');
+    cy.tick(2000);
   });
 });
