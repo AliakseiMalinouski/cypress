b4fa61cea8b5573aef5e2c87ef6266dfd8030916
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-07-28 14:48:56 +0200

    cypress fixtures

--- cypores-module-4/cypress/e2e/location.cy.js
+++ cypores-module-4/cypress/e2e/location.cy.js
@@ -2,23 +2,21 @@
 
 describe('share location', () => {
   beforeEach(() => {
+    cy.fixture('user_location.json').as('userLocation');
     cy.visit('/').then((win) => {
-      if('navigator' in win) {
-        cy.stub(window.navigator.geolocation, 'getCurrentPosition').as('getUserPosition')
-        .callsFake((cb) => {
-          setTimeout(() => {
-            cb({
-              coords: {
-                latitude: 0.1,
-                longitude: 0.1,
-              }
-            });
-          }, 100);
-        });
-        cy.stub(window.navigator.clipboard, 'writeText')
-        .as('copyToClipboard')
-        .resolves();
-      }
+      cy.get('@userLocation').then((params) => {
+        if('navigator' in win) {
+          cy.stub(window.navigator.geolocation, 'getCurrentPosition').as('getUserPosition')
+          .callsFake((cb) => {
+            setTimeout(() => {
+              cb(params);
+            }, 100);
+          });
+          cy.stub(window.navigator.clipboard, 'writeText')
+          .as('copyToClipboard')
+          .resolves();
+        }
+      });
     });
   })
   it('should fetch the user location', () => {
